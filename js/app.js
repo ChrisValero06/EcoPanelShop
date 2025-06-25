b03647eb991d56cca8bd7b492655dafe69851516
christopher valero <151199440+ChrisValero06@users.noreply.github.com>
2025-06-25 14:54:58 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-if("serviceWorker" in navigator){
-    window.addEventListener("load", function(){
-        this.navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err));
-    });
-}
+if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('../serviceWorker.js')
+          .then(reg => console.log('Service Worker registrado:', reg.scope))
+          .catch(err => console.error('Error al registrar SW:', err));
+      });
+    }
